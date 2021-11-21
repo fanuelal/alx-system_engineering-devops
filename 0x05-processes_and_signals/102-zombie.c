6a94cf2f2de48501fe22a0920a992db9527d2ca4
fanuelal <fanuelalmawstop.com>
2021-11-21 14:23:10 -0800

    zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ *infinite_while - creates infinite loop
+ *Return: always 0
+ */
+int infinite_while(void)
+{
+while (1)
+{
+sleep(1);
+}
+return (0);
+}
+/**
+ * main - For every zombie process created, it displays
+ * Return: always 0
+ */
+int main(void)
+{
+int index = 0;
+pid_t zomb;
+while (index < 5)
+{
+zomb = fork();
+if (!zomb)
+return (0);
+printf("Zombie process created, PID: %d\n", zomb);
+}
+infinite_while();
+return (0);
+}
