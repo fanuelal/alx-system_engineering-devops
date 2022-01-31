f05c6c0107437b0cd45311501a87393eebd68480
fanuelal <fanuelalmawstop@gmail.com>
2022-01-31 12:59:20 -0800

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script to export data in the CSV format"""
+
+
+import requests
+import json
+import sys
+import csv
+if __name__ == "__main__":
+
+    uid = sys.argv[1]
+    url = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                       .format(uid))
+    name = url.json().get('name')
+    tod = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totaltasks = 0
+    completed = 0
+    
+    fileName = "{}.csv".format(uid)
+    with open(fileName, 'w') as csvfile:
+        obj = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in tod.json():
+            if task.get('userId') == int(uid):
+                obj.writerow([uid, name, str(task.get('completed')), task.get('title')])
