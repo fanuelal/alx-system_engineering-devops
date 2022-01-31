20b7618b410c3de5dd5e5ea914b17aa970bfa0d5
fanuelal <fanuelalmawstop@gmail.com>
2022-01-31 13:05:55 -0800

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,10 +15,11 @@ if __name__ == "__main__":
     tod = requests.get('https://jsonplaceholder.typicode.com/todos')
     totaltasks = 0
     completed = 0
-    
     fileName = "{}.csv".format(uid)
     with open(fileName, 'w') as csvfile:
-        obj = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n')
+        obj = csv.writer(csvfile, delimiter=',', quotechar='"',
+                         quoting=csv.QUOTE_ALL, lineterminator='\n')
         for task in tod.json():
             if task.get('userId') == int(uid):
-                obj.writerow([uid, name, str(task.get('completed')), task.get('title')])
+                obj.writerow([uid, name,
+                              str(task.get('completed')), task.get('title')])
