90b887f97245759e9ba48fad12e63fbbedcd7011
fanuelal <fanuelalmawstop@gmail.com>
2022-02-15 12:19:28 -0800

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,16 @@
 """Module for task"""
 
 import requests
+
+
 def number_of_subscribers(subreddit):
-    """a function that queries the Reddit API and returns the number of subscribers """
+    """a function that queries the Reddit API
+and returns the number of subscribers """
 
     retrived = requests.get("https:/www.reddit.com/r/{}/about.json".
-                           format(subreddit),allow_redirects=False)
-    if retrived.status_code is not 200:
+                            format(subreddit),
+                            headers={"User-Agent": "KIDIST-123"})
+    if retrived.status_code != 200:
         return 0
     else:
         return retrived.json().get("data").get("subscribers")
-    
