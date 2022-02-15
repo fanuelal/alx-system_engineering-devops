047e5ee12670191f5a2d5eca45dce3fbfc75e0a1
fanuelal <fanuelalmawstop@gmail.com>
2022-02-15 11:49:44 -0800

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes a wordpress site
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
